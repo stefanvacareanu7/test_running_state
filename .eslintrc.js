c2c625c75bcfb195bde9f2bdb561e63c7002cf80
stefanvacareanu7 <91726880+stefanvacareanu7@users.noreply.github.com>
2021-11-18 18:05:47 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
-{
-  "root": true,
-  "env": {
-    "node": true
-  },
-  "plugins": ["cypress", "prettier"],
-  "extends": ["plugin:cypress/recommended", "prettier"],
-  "rules": {
-    "cypress/no-assigning-return-values": "warn",
-    "cypress/no-unnecessary-waiting": "warn",
-    "cypress/assertion-before-screenshot": "warn",
-    "cypress/no-force": "warn",
-    "cypress/no-async-tests": "warn",
-    "prettier/prettier": "warn",
-    "no-var": "warn"
+"root": true,
+"env": {
+  "node": true
+},
+"plugins": ["cypress", "prettier"],
+"extends": ["plugin:cypress/recommended", "prettier"],
+"rules": {
+  "cypress/no-assigning-return-values": "warn",
+  "cypress/no-unnecessary-waiting": "warn",
+  "cypress/assertion-before-screenshot": "warn",
+  "cypress/no-force": "warn",
+  "cypress/no-async-tests": "warn",
+  "prettier/prettier": "warn",
+  "no-var": "warn"
   }
-}
